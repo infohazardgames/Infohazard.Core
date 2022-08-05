f06441036482411eb7b49ff3610fb87ab57193ed
Vincent Miller <vmiller0725@gmail.com>
2022-08-05 00:39:21 -0700

    License headers

--- Runtime/Misc/SceneControl.cs
+++ Runtime/Misc/SceneControl.cs
@@ -1,6 +1,6 @@
-﻿// MIT License
+﻿// The MIT License (MIT)
 // 
-// Copyright (c) 2020 Vincent Miller
+// Copyright (c) 2022-present Vincent Miller
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
