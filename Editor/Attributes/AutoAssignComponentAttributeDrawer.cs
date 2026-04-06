865892e06c8f2a7b0479be2729365c031a6be5ce
Val Miller <infohazard.games@gmail.com>
2026-04-05 17:09:36 -0700

    Added a component to auto assign references

---
+++ Editor/Attributes/AutoAssignComponentAttributeDrawer.cs
@@ -0,0 +1,29 @@
+﻿// This file is part of the Infohazard.Core package.
+// Copyright (c) 2022-present Vincent Miller (Infohazard Games).
+
+// This file is part of the Infohazard.Core package.
+// Copyright (c) 2026-present Vincent Miller (Infohazard Games).
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Infohazard.Core {
+    [CustomPropertyDrawer(typeof(AutoAssignComponentAttribute))]
+    public class AutoAssignComponentAttributeDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.PropertyField(position, property, label);
+            if (property.propertyType != SerializedPropertyType.ObjectReference ||
+                property.objectReferenceValue != null) {
+                return;
+            }
+            
+            Component component = property.serializedObject.targetObject as Component;
+            if (component == null) return;
+            Type fieldType = fieldInfo.FieldType;
+            if (fieldType.IsInterface || typeof(Component).IsAssignableFrom(fieldType)) {
+                property.objectReferenceValue = component.GetComponent(fieldType);
+            }
+        }
+    }
+}
---
+++ Runtime/Attributes/AutoAssignComponentAttribute.cs
@@ -0,0 +1,9 @@
+﻿// This file is part of the Infohazard.Core package.
+// Copyright (c) 2026-present Vincent Miller (Infohazard Games).
+
+using UnityEngine;
+
+namespace Infohazard.Core {
+    public class AutoAssignComponentAttribute : PropertyAttribute {
+    }
+}
